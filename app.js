e60e4b1e4bf950ff9d5e914529245b8f84a69a3c
Hatooz <hatem.mohieldin@gmail.com>
2020-10-30 15:31:22 +0100

    adding more cors, playing with settings

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-
-io.origins(['https://me-vue.elbizza.me/#/chat']);
+// io.origins(['https://me-vue.elbizza.me/#/chat']);
 io.on("connection", socket => {
     console.log("user connected");  
     socket.on('message', function (message) {
