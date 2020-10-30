d7855e4bf4b868fb17996e045a0b9143e2777b99
Hatooz <hatem.mohieldin@gmail.com>
2020-10-30 15:17:06 +0100

    adding more cors, playing with settings

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(cors());
+// app.use(cors());
 
 
 io.origins(['https://me-vue.elbizza.me']);
