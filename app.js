065d8f45e87b696a8a4a02fc8ec51c6c7c1db015
Hatooz <hatem.mohieldin@gmail.com>
2020-10-30 14:51:00 +0100

    adding more cors

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const cors = require('cors');
 
 
 app.use(cors());
-
+//ENABLE CORS
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
 socketio.origins(['https://me-chat.elbizza.me:443']);
 socketio.on("connection", socket => {
     console.log("user connected");  
